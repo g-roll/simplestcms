09d207b0a53ca14c96cfb94c6954924050b95c32
g-roll <83816302+g-roll@users.noreply.github.com>
2022-06-02 19:12:18 +0000

    Adding dynamic data mapping support

--- index.js
+++ index.js
@@ -1,57 +1,86 @@
 #!/usr/bin/env node
-import {readFileSync, readdirSync, lstatSync} from 'fs'
+
+import {readFileSync, readdirSync, lstatSync, existsSync} from 'fs'
 import {outputFile} from 'fs-extra'
 import {program} from 'commander'
 import Handlebars from 'handlebars'
 
 program
-  .option('-s, --source <path/to/source/directory>')
-  .option('-t, --target <path/to/target/directory>')
-  .option('-d, --data <path/to/file.json>')
-  .description('The simplest CMS to simplify your everyday life as a web developer. Specify a source and destination directory of your HTML code, as well as a data source file in JSON format. Include some handlebars expressions in your HTML source code to fetch the data from the JSON file and generate a static site on the fly 🚀 You can find more information at github.com/g-roll/simplestcms.')
-  .usage('-s <path/to/source/directory> -t <path/to/target/directory> -d <path/to/file.json>')
+  .option('-s, --source <path/to/source/dir>', 'input directory')
+  .option('-t, --target <path/to/target/dir>', 'output directory')
+  .option('-d, --data <path/to/file.json>', '[optional] global data file')
+  .description('Specify a source and destination directory of your HTML/XML code. Handlebars expressions are dynamically resolved using the JSON file of the corresponding filename (e.g. index.html.json), optionally with a declared global data JSON file.. More information at github.com/g-roll/simplestcms.')
+  .usage('-s <path/to/source/dir> -t <path/to/target/dir>')
 program.parse()
 
-const dataJSON = await JSON.parse(readFileSync(program.opts().data),'utf8')
 const templateList = []
 const partialList = []
 
-console.time('Done 🎉 Completed in')
-async function main(sourceDir, targetDir, _dataSrc) {
-  
-  readdirSync(sourceDir).forEach(file => {
-    let filePath = sourceDir+file
+async function dataOption() {
+  if (existsSync(program.opts().data)) {
+      let result = await JSON.parse(readFileSync(program.opts().data), 'utf8')
 
-    if (lstatSync(filePath).isDirectory()) {
-      main(filePath+'/', targetDir, _dataSrc)
-    }
+      return result
+  } else {
+      let result = null
 
-    else if (/^.*((\.html)|(\.xml)).*$/.test(filePath)) {
-      if (/^.*(\.partial).*$/.test(filePath)) {
-        partialList.push(filePath)
+      return result
+  }
+}
+
+async function fileLookup(sourceDir) {
+  readdirSync(sourceDir).forEach(async function(file) {
+      let filePath = sourceDir + file
+
+      if (lstatSync(filePath).isDirectory()) {
+          await fileLookup(filePath + '/')
+      } else if (/^.*((\.html)|(\.xml))$/.test(filePath)) {
+          if (/^.*(\.partial).*$/.test(filePath)) {
+              partialList.push(filePath)
+          } else {
+              templateList.push(filePath)
+          }
       }
-
-      else {
-        templateList.push(filePath)
-      }
-    }
-  })
-
-  partialList.forEach(partial => {
-    let content = Handlebars.compile(readFileSync(partial,'utf8'))
-    let partialName = partial.split(program.opts().source+'/').pop()
-
-    Handlebars.registerPartial(partialName, content(dataJSON))
-  })
-
-  templateList.forEach(template => {
-    let content = Handlebars.compile(readFileSync(template,'utf8').toString())
-    let relPath = targetDir+template.split(program.opts().source+'/').pop()
-
-    outputFile(relPath, content(dataJSON))
-    console.log(relPath+' generated')
   })
 }
 
-await main(program.opts().source+'/', program.opts().target+'/', program.opts().data)
-console.timeEnd('Done 🎉 Completed in')
+async function partialRegistration(dataSpread) {
+  partialList.forEach(async function(partial) {
+      let partialContents = Handlebars.compile(readFileSync(partial, 'utf8'))
+
+      Handlebars.registerPartial(partial.split(program.opts().source + '/').pop(), partialContents(dataSpread))
+  })
+}
+
+console.time('[simplestcms]: Done 🎉 Completed in')
+async function main(source, target, _dataSrc) {
+  const dataGlobal = await dataOption()
+
+  await fileLookup(source)
+
+  templateList.forEach(async function(template) {
+      let templateContents = Handlebars.compile(readFileSync(template, 'utf8').toString())
+      let relPath = target + await template.split(program.opts().source + '/').pop()
+
+      if (existsSync(template + '.json')) {
+          let dataLocal = await JSON.parse(readFileSync(template + '.json', 'utf8'))
+
+          partialRegistration({
+              ...dataGlobal,
+              ...dataLocal
+          }) // await was not used because a promise would have led to a distorted result
+
+          await outputFile(relPath, templateContents({
+              ...dataGlobal,
+              ...dataLocal
+          }))
+      } else {
+          partialRegistration(dataGlobal)
+          await outputFile(relPath, templateContents(dataGlobal))
+      }
+  })
+}
+
+await main(program.opts().source + '/', program.opts().target + '/', program.opts().data)
+console.log('[simplestcms]: {' + templateList.map(e => ['\n'+e]) + '\n} generated.')
+console.timeEnd('[simplestcms]: Done 🎉 Completed in')
